3ed8784a8785250b7008d1536744c708f9a06d04
Gerry Seidman <gerry@swatt.exchange>
2018-05-29 13:37:41 -0400

    Resolving CollectionsUtilities conflict

--- src/main/java/com/swatt/util/general/CollectionsUtilities.java
+++ src/main/java/com/swatt/util/general/CollectionsUtilities.java
@@ -82,6 +82,18 @@ public class CollectionsUtilities {
         
         return map;
     }
+
+    
+	public static final boolean equals(Object obj1, Object obj2) {
+		if (obj1 == null) {
+			return (obj2 == null);
+		} else {
+			if (obj2 != null)
+				return obj1.equals(obj2);
+			else 
+				return false;
+		}
+	}
     
 //	public static Collection add(Collection collection, Object array[]) {
 //	if (array == null)
