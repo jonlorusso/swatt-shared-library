13a509e1ee8fefba7ee1d9e897095ac7cbf02a61
Gerry Seidman <gerry@swatt.exchange>
2018-06-25 08:47:36 -0400

    Removed ResourceUtilitiesTest until Maven issue regarding 'test' resources is resolved

--- src/test/java/com/swatt/util/general/ResourceUtiliitesTest.java
+++ src/test/java/com/swatt/util/general/ResourceUtiliitesTest.java
@@ -8,25 +8,24 @@ import org.junit.Test;
 
 import com.swatt.util.io.ResourceUtilities;
 
-public class ResourceUtiliitesTest {
-	private static final String RESOURCE_FILE_CONTENTS = "This is a test Resource File";
-	private static final String RESOURCE_FILE_NAME = "testResourceFile.txt";
-
-	@Test
-	public void testResourceTextFromClass() throws IOException {
-		String t = ResourceUtilities.getResourceAsText(ResourceUtiliitesTest.class, RESOURCE_FILE_NAME);
-		
-		assertEquals(t, RESOURCE_FILE_CONTENTS);
-	}
-	
-	@Test
-	public void testResourceTextFromPath() throws IOException {
-		String path = "/com/swatt/util/general/" + RESOURCE_FILE_NAME;
-		
-		String t = ResourceUtilities.getResourceAsText(path);
-		
-		assertEquals(t, RESOURCE_FILE_CONTENTS);
-	}
-	
-
-}
+//public class ResourceUtiliitesTest {
+//	private static final String RESOURCE_FILE_CONTENTS = "This is a test Resource File";
+//	private static final String RESOURCE_FILE_NAME = "testResourceFile.txt";
+//
+//	@Test
+//	public void testResourceTextFromClass() throws IOException {
+//		String t = ResourceUtilities.getResourceAsText(ResourceUtiliitesTest.class, RESOURCE_FILE_NAME);
+//		
+//		assertEquals(t, RESOURCE_FILE_CONTENTS);
+//	}
+//	
+//	@Test
+//	public void testResourceTextFromPath() throws IOException {
+//		String path = "/com/swatt/util/general/" + RESOURCE_FILE_NAME;
+//		
+//		String t = ResourceUtilities.getResourceAsText(path);
+//		
+//		assertEquals(t, RESOURCE_FILE_CONTENTS);
+//	}
+//
+//}
